6f6d6859156577a27053de6302a48d192e21f751
DevAman-sudo <devaman.net@gmail.com>
2021-03-08 21:22:05 +0545

    request pull data from form

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,19 @@
 // DOM elements //
 const readMoreBtn = document.getElementById('read_more_btn');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const submitBtn = document.getElementById('submit_btn');
 
+// redirection button //
 readMoreBtn.addEventListener('click' , () => {
     window.location = '/about';
+});
+
+// form submit //
+form.addEventListener('submit' , (event) => {
+    event.preventDefault();
+    
+    alert(username.value);
 });
