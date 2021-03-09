f09308dc42ebe744f95d3c3ea4159e5b0cbef911
DevAman-sudo <devaman.net@gmail.com>
2021-03-09 15:49:20 +0545

    SMTP JS

--- public/js/main.js
+++ public/js/main.js
@@ -7,13 +7,25 @@ const password = document.getElementById('password');
 const submitBtn = document.getElementById('submit_btn');
 
 // redirection button //
-readMoreBtn.addEventListener('click' , () => {
+readMoreBtn.addEventListener('click', () => {
     window.location = '/about';
 });
 
 // form submit //
-form.addEventListener('submit' , (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    
+
     alert(username.value);
+
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "username",
+        Password: "password",
+        To: 'devaman.net@gmail.com',
+        From: "you@isp.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
 });
